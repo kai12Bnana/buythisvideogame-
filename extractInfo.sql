ac989c06202ff9edaaa92fa8549dfde6caf457df
kai12Bnana <42561406+kai12Bnana@users.noreply.github.com>
2019-07-29 23:07:51 +1000

    Update extractInfo.sql

--- extractInfo.sql
+++ extractInfo.sql
@@ -1,5 +1,5 @@
 CREATE TABLE VG_Basicinfo (
-	vg_year int NOT NULL,
+	vg_name varchar(50) NOT NULL, vg_year int NOT NULL,
 	vg_type varchar(50) NOT NULL,
 	vg_nasale int NOT NULL,
 	vg_eusale int NOT NULL,
@@ -8,6 +8,5 @@ CREATE TABLE VG_Basicinfo (
 	vg_glbsale int NOT NULL,
 );
 
-INSERT INTO VG_Basicinfo (vg_year, vg_type, vg_nasale, vg_eusale, vg_jpsale, vg_othersale, vg_glbsale) SELECT Year, Genre, NA_Sales, EU_Sales, JP_Sales, Other_Sales, Global_Sales FROM vgsalesinyears
-												                     WHERE vgname = vgid1
-												                       AND vgplatform = vgid2;
+INSERT INTO VG_Basicinfo (vg_name, vg_year, vg_type, vg_nasale, vg_eusale, vg_jpsale, vg_othersale, vg_glbsale) SELECT Name, Year, Genre, NA_Sales, EU_Sales, JP_Sales, Other_Sales, Global_Sales FROM vgsalesinyears
+												                     WHERE vgplatform = vginput;
